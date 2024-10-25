6cf0d6030fbec0a3ac54878113bf000128b23f65
Livia Alves de Faria <livia.faria21@gmail.com>
2024-10-25 10:42:30 -0300

    ajuste css da lista

--- frontend/pages/listas/listas.tsx
+++ frontend/pages/listas/listas.tsx
@@ -255,7 +255,7 @@ const styles = StyleSheet.create({
         right: 20,
         bottom: 20,
         backgroundColor: '#FC7493',
-        borderRadius: 50,
+        borderRadius: 13,
         width: 60,
         height: 60,
         justifyContent: 'center',
